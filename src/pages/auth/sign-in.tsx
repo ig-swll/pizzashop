b248c4750010c5308e6e7e646bd0cd848be10095
Matheus Bezerra <matheus.bezerra@senado.leg.br>
2024-05-14 19:13:19 -0300

    adiciona página de cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Helmet } from 'react-helmet-async';
 import { type SubmitHandler, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -23,14 +25,29 @@ export function SignIn() {
   });
 
   const handleSignIn: SubmitHandler<SignInForm> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(data);
+    try {
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Tentar novamente',
+          onClick: () => {},
+        },
+      });
+    } catch {
+      toast.error('Credenciais inválidas');
+    }
   };
 
   return (
     <>
       <Helmet title="Login" />
+
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
